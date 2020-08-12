ead9937e99fe64ec5c7ccdff70b9cae3ef8d53ff
Karen Lei <karenwinnielei@gmail.com>
2020-08-12 00:01:54 -0700

    complete doubly linked list

---
+++ singly_linked_list/benchmark_singly_linked_list.py
@@ -0,0 +1,36 @@
+import time
+from singly_linked_list import LinkedList
+
+n = 100000
+
+l = []
+ll = LinkedList()
+
+for i in range(n):
+  l.append(i)
+  ll.add_to_tail(i)
+
+start_time = time.time()
+for i in range(n):
+  l.append(i) #0(1)
+end_time = time.time()
+print(f"Adding {n} elements to list took {end_time - start_time} seconds")
+
+start_time = time.time()
+for i in range(n):
+  ll.add_to_tail(i) #0(1)
+end_time = time.time()
+print(
+    f"Adding {n} elements to linked list took {end_time - start_time} seconds")
+
+start_time = time.time()
+for i in range(n):
+  l.pop(0)
+end_time = time.time()
+print(f"List pop from front took {end_time - start_time} seconds")
+
+start_time = time.time()
+for i in range(n):
+  ll.remove_head()
+end_time = time.time()
+print(f"Linked list remove from front took {end_time - start_time} seconds")
